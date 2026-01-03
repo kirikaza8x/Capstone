0aa4456323290559a816fcfea8c7930c0152483e
machgiahao <machgiahao0979@gmail.com>
2026-01-03 13:10:47 +0700

    First commit

---
+++ Shared/Shared.Application/Messaging/IEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Shared.Domain.DDD;
+
+namespace Shared.Application.Messaging;
+
+public interface IEventHandler<TEvent> : INotificationHandler<TEvent>
+    where TEvent : IDomainEvent
+{
+}
---
+++ Shared/Shared.Domain/DDD/IAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Domain.DDD;
+
+public interface IAggregateRoot<T> : IAggregateRoot, IEntity<T>
+{
+}
+
+public interface IAggregateRoot : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
+}
