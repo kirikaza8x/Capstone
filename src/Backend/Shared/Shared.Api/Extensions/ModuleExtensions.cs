d919ddbe7779ca88291d55a111645471a10610ad
machgiahao <machgiahao0979@gmail.com>
2026-01-08 01:00:08 +0700

    Add carter extension

---
+++ src/Backend/Shared/Shared.Api/Extensions/ModuleExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Api.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Api.Extensions;
+
+public static class ModuleExtensions
+{
+    private static readonly List<IModule> RegisteredModules = new();
+
+    public static IServiceCollection AddModule<TModule>(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        where TModule : IModule, new()
+    {
+        var module = new TModule();
+        module.RegisterModule(services, configuration);
+        RegisteredModules.Add(module);
+        return services;
+    }
+
+    public static WebApplication MapModules(this WebApplication app)
+    {
+        foreach (var module in RegisteredModules)
+        {
+            module.MapEndpoints(app);
+        }
+        return app;
+    }
+}
---
+++ src/Backend/Shared/Shared.Api/Modules/IModule.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Api.Modules;
+
+public interface IModule
+{
+    IServiceCollection RegisterModule(IServiceCollection services, IConfiguration configuration);
+    IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints);
+}
