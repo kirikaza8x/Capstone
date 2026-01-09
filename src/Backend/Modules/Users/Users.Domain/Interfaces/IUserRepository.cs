30d7720f4bd34ae0b4c3ca67f3fb907c84b34ae0
tantttse <156907341+tantttse@users.noreply.github.com>
2026-01-09 11:13:31 +0700

    define Repo for user module

---
+++ src/Backend/Modules/Users/Users.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+using Users.Domain.Entities;
+using Shared.Domain.Data;
+
+namespace Users.Domain.Repositories
+{
+    public interface IUserRepository : IRepository<User, Guid>
+    {
+        Task<User?> GetByUserNameAsync(string userName, CancellationToken cancellationToken = default);
+        Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+        Task<User?> LoginAsync(string userNameOrEmail, string password, CancellationToken cancellationToken = default);
+        Task<User?> GetUserByMailOrUserName(string userNameOrEmail, CancellationToken cancellationToken = default);
+        Task<User?> GetUserByMailOrUserName(
+            IEnumerable<string> userNamesOrEmails,
+            CancellationToken cancellationToken = default);
+
+    }
+}
