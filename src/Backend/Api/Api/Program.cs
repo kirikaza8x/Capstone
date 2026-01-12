d397855c837abec5031baa8b0409b86bb0c4147c
tantttse <156907341+tantttse@users.noreply.github.com>
2026-01-12 21:36:45 +0700

    init Users Modules

--- src/Backend/Api/Api/Program.cs
+++ src/Backend/Api/Api/Program.cs
@@ -1,3 +1,6 @@
+using Carter;
+using Shared.Api.Extensions;
+using Users.Api;
 
 namespace Api
 {
@@ -6,25 +9,25 @@ namespace Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
-
+            builder.Services.AddCarterWithAssemblies(
+                typeof(UsersApiAssemblyReference).Assembly
+            );
+            builder.Services.AddModule<UserModule>(builder.Configuration);
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
+            app.MapCarter();
+
+            app.MapModules();
 
             app.MapControllers();
 
