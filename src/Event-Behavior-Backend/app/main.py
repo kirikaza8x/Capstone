1f678abfe2542814f9428d7eeb22bd642584ddd9
tantttse <156907341+tantttse@users.noreply.github.com>
2026-01-23 15:43:57 +0700

    add fast api for user analyzing behavior

---
+++ src/Event-Behavior-Backend/app/main.py
@@ -0,0 +1,21 @@
+# app/main.py
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+# Import the router here (this line was missing or wrong)
+from app.api.v1.logs import router as logs  
+
+from app.database import engine, Base
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+app = FastAPI(
+    title="Event AI – Recommendation Backend",
+    lifespan=lifespan
+)
+
+app.include_router(logs, prefix="/api/v1")  
