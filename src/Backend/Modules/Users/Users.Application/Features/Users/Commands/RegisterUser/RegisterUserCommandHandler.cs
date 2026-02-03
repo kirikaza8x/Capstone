b321289a35a503ca40d1b61b60257b2ab34bd27f
tantttse <156907341+tantttse@users.noreply.github.com>
2026-02-03 11:46:50 +0700

    split user validators

--- src/Backend/Modules/Users/Users.Application/Features/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
+++ src/Backend/Modules/Users/Users.Application/Features/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -11,38 +11,6 @@ using Users.Domain.UOW;
 
 namespace Users.Application.Features.Users.Commands.RegisterUser
 {
-    public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
-    {
-        public RegisterUserCommandValidator()
-        {
-            RuleFor(x => x.Email)
-                .NotEmpty().WithMessage("Email is required.")
-                .EmailAddress().WithMessage("Email must be valid.");
-
-            RuleFor(x => x.UserName)
-                .NotEmpty().WithMessage("Username is required.")
-                .MaximumLength(100).WithMessage("Username must not exceed 100 characters.");
-
-            RuleFor(x => x.Password)
-                .NotEmpty().WithMessage("Password is required.")
-                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
-
-            RuleFor(x => x.FirstName)
-                .NotEmpty().WithMessage("First name is required.");
-
-            RuleFor(x => x.LastName)
-                .NotEmpty().WithMessage("Last name is required.");
-
-            RuleFor(x => x.PhoneNumber)
-                .MaximumLength(20)
-                .When(x => !string.IsNullOrWhiteSpace(x.PhoneNumber));
-
-            RuleFor(x => x.Address)
-                .MaximumLength(256)
-                .When(x => !string.IsNullOrWhiteSpace(x.Address));
-        }
-    }
-
     public class RegisterUserCommandHandler : ICommandHandler<RegisterUserCommand, UserResponseDto>
     {
         private readonly IUserRepository _userRepository;
