f8651594d3e915bc7d1d90b5cd361162694a1eee
tantttse <156907341+tantttse@users.noreply.github.com>
2026-03-12 16:43:47 +0700

    update: namespace for public api

--- src/Backend/Modules/Users/Users.Infrastructure/Persistence/Seeds/RoleSeeder.cs
+++ src/Backend/Modules/Users/Users.Infrastructure/Persistence/Seeds/RoleSeeder.cs
@@ -15,17 +15,17 @@ public class RoleSeeder(UserModuleDbContext context) : IDataSeeder<Role>
 
         var rolesToAdd = new List<Role>();
 
-        if (!existingRoles.Contains(PublicApi.Constants.Roles.Admin))
-            rolesToAdd.Add(Role.Create(PublicApi.Constants.Roles.Admin, "System Administrator"));
+        if (!existingRoles.Contains(Users.PublicApi.Constants.Roles.Admin))
+            rolesToAdd.Add(Role.Create(Users.PublicApi.Constants.Roles.Admin, "System Administrator"));
 
-        if (!existingRoles.Contains(PublicApi.Constants.Roles.Staff))
-            rolesToAdd.Add(Role.Create(PublicApi.Constants.Roles.Staff, "Staff"));
+        if (!existingRoles.Contains(Users.PublicApi.Constants.Roles.Staff))
+            rolesToAdd.Add(Role.Create(Users.PublicApi.Constants.Roles.Staff, "Staff"));
 
-        if (!existingRoles.Contains(PublicApi.Constants.Roles.Organizer))
-            rolesToAdd.Add(Role.Create(PublicApi.Constants.Roles.Organizer, "Event Organizer"));
+        if (!existingRoles.Contains(Users.PublicApi.Constants.Roles.Organizer))
+            rolesToAdd.Add(Role.Create(Users.PublicApi.Constants.Roles.Organizer, "Event Organizer"));
 
-        if (!existingRoles.Contains(PublicApi.Constants.Roles.Attendee))
-            rolesToAdd.Add(Role.Create(PublicApi.Constants.Roles.Attendee, "Attendee"));
+        if (!existingRoles.Contains(Users.PublicApi.Constants.Roles.Attendee))
+            rolesToAdd.Add(Role.Create(Users.PublicApi.Constants.Roles.Attendee, "Attendee"));
 
         if (rolesToAdd.Count > 0)
         {
